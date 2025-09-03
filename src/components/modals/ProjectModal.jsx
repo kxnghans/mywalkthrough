169ad3a14d67794d1aeaaeca966e7d2fa4deba69
kobbyhanson <89821838+kobbyhanson@users.noreply.github.com>
2025-09-03 00:47:51 -0500

    feat: Update project modal buttons and add live link

--- src/components/modals/ProjectModal.jsx
+++ src/components/modals/ProjectModal.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { CloseIcon } from "../icons/Icons";
+import { FaCirclePlay } from "react-icons/fa6";
 
 const ProjectModal = ({ project, onClose }) => {
   const modalRef = useRef();
@@ -84,9 +85,10 @@ const ProjectModal = ({ project, onClose }) => {
               href={project.liveLink || "#"}
               target="_blank"
               rel="noopener noreferrer"
-              className="bevel-button-dark transform rounded-lg bg-red-600 px-4 py-2 font-bold text-white transition-all duration-200 hover:bg-red-700"
+              className="bevel-button-dark flex items-center space-x-2 transform rounded-lg bg-red-600 px-4 py-2 font-bold text-white transition-all duration-200 hover:bg-red-700"
             >
-              Live Project
+              <FaCirclePlay />
+              <span>Demo</span>
             </a>
             <a
               href={project.codeLink || "#"}
@@ -94,7 +96,7 @@ const ProjectModal = ({ project, onClose }) => {
               rel="noopener noreferrer"
               className="bevel-button-dark transform rounded-lg bg-gray-700 px-4 py-2 font-bold text-white transition-all duration-200 hover:bg-gray-800"
             >
-              Code Repository
+              View Project
             </a>
           </div>
         </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,5 @@
 export const projectData = [
-  {
+    {
     title: "Portfolio Showcase",
     imageUrl: "https://images.unsplash.com/photo-1607469256721-13472920da4e?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Placeholder
     summary: [
@@ -28,7 +28,7 @@ export const projectData = [
         },
       ],
     },
-  },
+  },,
   {
     title: "Facilities Automation",
     imageUrl:
